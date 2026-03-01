183455ad3ba07450b452189f301bdec777bfc4a8
AndersT <anders.thorsrud@rubic.no>
2026-03-01 18:33:20 +0100

    rewrote the entire thing

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,132 +1,27 @@
---
--- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
--- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
--- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
--- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
--- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
--- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
---
--- File: mappings.lua
--- Description: Key mapping configs
--- Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
+local map = vim.keymap.set
 
--- <leader> is a space now
-local map   = vim.keymap.set
-local lsp   = vim.lsp.buf
-local utils = require("utils")
-
--- Reload configuration without restart nvim
-map("n", "<leader>r", ":source $MYVIMRC<CR>", { desc = "Reload configuration without restart nvim" })
+-- Clear search highlight
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Telescope
 local builtin = require("telescope.builtin")
-map("n", "<leader>ff", builtin.find_files, { desc = "Open Telescope to find files" })
-map("n", "<leader>fg", builtin.live_grep, { desc = "Open Telescope to do live grep" })
-map("n", "<leader>fb", builtin.buffers, { desc = "Open Telescope to list buffers" })
-map("n", "<leader>fh", builtin.help_tags, { desc = "Open Telescope to show help" })
-map("n", "<leader>fo", builtin.oldfiles, { desc = "Open Telescope to list recent files" })
-map("n", "<leader>cm", builtin.git_commits, { desc = "Open Telescope to list git commits" })
-map("n", "<leader>rf", builtin.lsp_references, { desc = "Open Telescope to list references" })
+map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+map("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+map("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+map("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+map("n", "<leader>fo", builtin.oldfiles, { desc = "Recent files" })
 
 -- LSP
-map("n", "<leader>pf", function()
-  require("plf").format { lsp_fallback = true }
-end, { desc = "General Format file" })
-
+local lsp = vim.lsp.buf
 map('n', '<leader>jd', lsp.definition, {})
 map('n', '<leader>sd', lsp.hover, {})
 map('n', '<leader>fc', lsp.code_action, {})
 map('n', '<leader>rn', lsp.rename, {})
 map('n', '<leader>se', vim.diagnostic.open_float, {})
 
--- global lsp mappings
-map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic loclist" })
-
--- Comment
-map("n", "mm", "gcc", { desc = "Toggle comment", remap = true })
-map("v", "mm", "gc", { desc = "Toggle comment", remap = true })
-
--- Windows
-map("n", "<F6>", "<cmd>vertical resize +5<CR>", { desc = "Resize window -vertical" })
-map("n", "<F9>", "<cmd>vertical resize -5<CR>", { desc = "Resize window +vertical" })
-map("n", "<F7>", "<cmd>horizontal resize +5<CR>", { desc = "Resize window -horizontal" })
-map("n", "<F8>", "<cmd>horizontal resize -5<CR>", { desc = "Resize window +horizontal" })
-
--- Move lines
-map("v", "J", "dp`[=`]`[V`]")
-map("v", "K", "dkP`[=`]`[V`]")
-
--- Indent lines
-map("v", "<", "<`[V`]")
-map("v", ">", ">`[V`]")
-
--- Move view
--- map("n", "<C-d>", "<C-d>zz")
--- map("n", "<C-u>", "<C-u>zz")
-
--- Clipboard
-map("x", "<leader>p", [["_dP"]])
-map({ "n", "v" }, "<leader>y", [["+y]])
-map("n", "<leader>Y", [["+Y]])
-map({ "n", "v" }, "<leader>d", "\"_d")
-
--- Harpoon
-local harpoon = require("harpoon")
-map("n", "<leader>hr", function() harpoon:list():add() end)
-map("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-map("n", "<C-h>", function() harpoon:list():select(1) end)
-map("n", "<C-j>", function() harpoon:list():select(2) end)
-map("n", "<C-k>", function() harpoon:list():select(3) end)
-map("n", "<C-l>", function() harpoon:list():select(4) end)
-
-map("n", "<leader>hn", function() harpoon:list():next() end)
-map("n", "<leader>hp", function() harpoon:list():prev() end)
-
 -- Oil
-map("n", "-", "<CMD>Oil<CR>", { desc = "open parent directory with oil"})
+map("n", "-", "<cmd>Oil<CR>", { desc = "Open file explorer" })
 
--- Terminal
-map("t", "<Esc>", "<C-\\><C-n>")
-map("n", "<leader>tr", "<cmd>split term://zsh<CR>")
-map("n", "<leader>tv", "<cmd>vsplit term://zsh<CR>")
-
--- dap
-local dap = require('dap')
-local adap = require('AndrrsDap')
-
-map('n', '<leader>dr', function() adap.start_debugger() end)
-map('n', '<leader>bp', function() dap.toggle_breakpoint() end)
-map('n', '<leader>dc', function() dap.continue() end)
-map('n', '<leader>so', function() dap.step_over() end)
-map('n', '<leader>si', function() dap.step_into() end)
-
-
--- copilot
--- F12 is easily accessible for me via a keyboard layer (I am not a psychopath)
-map('i', '<F12>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false
-})
-vim.g.copilot_no_tab_map = true
-
--- gp.nvim
-map('n', '<leader>gpt', '<cmd>GpChatToggle<CR>')
-map('n', '<leader>gpr', '<cmd>GpChatRespond<CR>')
-map('n', '<leader>gpn', '<cmd>GpChatNew<CR>')
-map('n', '<leader>gpd', '<cmd>GpChatDelete<CR>')
-map('v', '<leader>gpp', ':GpChatPaste<CR>')
-
--- terminal command
-map('n', '<F5>', function()
-  local command = vim.fn.input("Command: ")
-  if command == nil or command == "" then return end
-  vim.cmd("te {" .. command .. "}")
-end, { desc = "Run terminal command" })
-
--- Newlines at spaces
-map('n', 'K', "V:s/\\([\\[{(;,]\\) /\\1\\r/g | noh<CR>")
-
--- Open selection as a buffer (relative or absolute)
-map('v', '<C-a>', utils.go_to_selection)
-
+-- Easy align
+map("x", "ga", "<Plug>(EasyAlign)")
+map("n", "ga", "<Plug>(EasyAlign)")
---
+++ lua/plugins.lua
@@ -0,0 +1,161 @@
+return {
+	-- Colorscheme
+	{
+		"shaunsingh/nord.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("nord")
+		end,
+	},
+
+    -- Utilities
+    { "junegunn/vim-easy-align" },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    },
+    {
+        "jake-stewart/multicursor.nvim",
+        branch = "1.0",
+        config = function()
+            local mc = require("multicursor-nvim")
+            mc.setup()
+
+            local set = vim.keymap.set
+
+            -- Add or skip cursor above/below the main cursor.
+            set({"n", "x"}, "<M-k>", function() mc.lineAddCursor(-1) end)
+            set({"n", "x"}, "<M-j>", function() mc.lineAddCursor(1) end)
+            set({"n", "x"}, "<leader><M-k>", function() mc.lineSkipCursor(-1) end)
+            set({"n", "x"}, "<leader><M-j>", function() mc.lineSkipCursor(1) end)
+
+            -- Add or skip adding a new cursor by matching word/selection
+            set({"n", "x"}, "gl", function() mc.matchAddCursor(1) end)
+            set({"n", "x"}, "g>", function() mc.matchSkipCursor(1) end)
+            set({"n", "x"}, "gL", function() mc.matchAddCursor(-1) end)
+            set({"n", "x"}, "g<", function() mc.matchSkipCursor(-1) end)
+
+            -- Add and remove cursors with control + left click.
+            set("n", "<c-leftmouse>", mc.handleMouse)
+            set("n", "<c-leftdrag>", mc.handleMouseDrag)
+            set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+            -- Disable and enable cursors.
+            set({"n", "x"}, "<c-q>", mc.toggleCursor)
+
+            -- Mappings defined in a keymap layer only apply when there are
+            -- multiple cursors. This lets you have overlapping mappings.
+            mc.addKeymapLayer(function(layerSet)
+
+                -- Select a different cursor as the main one.
+                layerSet({"n", "x"}, "<left>", mc.prevCursor)
+                layerSet({"n", "x"}, "<right>", mc.nextCursor)
+
+                -- Delete the main cursor.
+                layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
+
+                -- Enable and clear cursors using escape.
+                layerSet("n", "<esc>", function()
+                    if not mc.cursorsEnabled() then
+                        mc.enableCursors()
+                    else
+                        mc.clearCursors()
+                    end
+                end)
+            end)
+
+            -- Customize how cursors look.
+            local hl = vim.api.nvim_set_hl
+            hl(0, "MultiCursorCursor", { reverse = true })
+            hl(0, "MultiCursorVisual", { link = "Visual" })
+            hl(0, "MultiCursorSign", { link = "SignColumn"})
+            hl(0, "MultiCursorMatchPreview", { link = "Search" })
+            hl(0, "MultiCursorDisabledCursor", { reverse = true })
+            hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+            hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+        end,
+    },
+
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+
+	-- Transparent background
+	{
+		"xiyaowong/transparent.nvim",
+		opts = {},
+	},
+
+	-- Git
+	{ "tpope/vim-fugitive" },
+    { "lewis6991/gitsigns.nvim" },
+
+	-- Icons
+	{ "nvim-tree/nvim-web-devicons" },
+
+	-- File explorer
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
+	},
+
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "python", "go", "rust" },
+			auto_install = true,
+			highlight = { enable = true },
+		},
+	},
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup()
+
+			local lspconfig = require("lspconfig")
+			local on_attach = function(_, bufnr)
+				local map = function(keys, func, desc)
+					vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+				end
+
+				map("<leader>jd", vim.lsp.buf.definition, "Go to definition")
+				map("<leader>sd", vim.lsp.buf.hover, "Hover documentation")
+				map("<leader>rf", vim.lsp.buf.references, "References")
+				map("<leader>fc", vim.lsp.buf.code_action, "Code actions")
+				map("<leader>rn", vim.lsp.buf.rename, "Rename")
+				map("<leader>se", vim.diagnostic.open_float, "Diagnostic float")
+				map("<leader>ds", vim.diagnostic.setloclist, "Diagnostic list")
+			end
+
+			require("mason-lspconfig").setup({
+				automatic_installation = true,
+				handlers = {
+					function(server_name)
+						lspconfig[server_name].setup({
+							on_attach = on_attach,
+						})
+					end,
+				},
+			})
+		end,
+	},
+}
