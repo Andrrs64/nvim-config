d5760a93b2f1e6e1772fa44e0b746c6008424b50
Anders Thorsrud <andersthorsrud@hotmail.no>
2026-03-12 23:03:11 +0100

    scale neovide

--- lua/options.lua
+++ lua/options.lua
@@ -41,4 +41,14 @@ if vim.g.neovide then
     vim.g.neovide_opacity                 = 0.9
     vim.g.neovide_normal_opacity          = 0.9
     vim.g.neovide_window_blurred          = true
+    vim.g.neovide_scale_factor = 1.0
+    local change_scale_factor = function(delta)
+        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+    end
+    vim.keymap.set("n", "<C-=>", function()
+        change_scale_factor(1.25)
+    end)
+    vim.keymap.set("n", "<C-->", function()
+        change_scale_factor(1/1.25)
+    end)
 end
